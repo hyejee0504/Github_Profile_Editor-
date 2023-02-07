e5d05c6d02770154d4add6e0853d2cd1bbe0c378
hyejihan <108308915+hyejee0504@users.noreply.github.com>
2023-02-07 10:24:27 +0900

    bug: align 배포 후 수정

--- my-app/src/components/Align.jsx
+++ my-app/src/components/Align.jsx
@@ -1,6 +1,6 @@
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import { useRef} from 'react';
-import {Checkboxinput, Typeheader, Selectwrapper, Inputwrap} from './style';
+import {Checkboxinput, Typeheader, Selectwrapper} from './style';
 
 export default function HeaderColor(props) {
   
@@ -31,9 +31,9 @@ const changealign = (e) =>{
     <Selectwrapper>
                     <Typeheader>전체적인 정렬을 선택해주세요.</Typeheader>
                     <div ref={ref1}>
-                      <Inputwrap><Checkboxinput type="checkbox" name="allleft"  onClick={changealign}/>전체 좌측 정렬</Inputwrap>
-                      <Inputwrap><Checkboxinput type="checkbox" name="allcenter"  onClick={changealign}/>전체 가운데 정렬</Inputwrap>
-                      <Inputwrap><Checkboxinput type="checkbox"  name="center" onClick={changealign}/>내용만 가운데 정렬</Inputwrap>
+                      <Checkboxinput type="checkbox" name="allleft"  onClick={changealign}/>전체 좌측 정렬
+                      <Checkboxinput type="checkbox" name="allcenter"  onClick={changealign}/>전체 가운데 정렬
+                      <Checkboxinput type="checkbox"  name="center" onClick={changealign}/>내용만 가운데 정렬
                     </div >  
                 </Selectwrapper>
   )
